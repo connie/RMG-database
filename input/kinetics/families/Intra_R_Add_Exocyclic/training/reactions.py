a039379c68c68e7b60da22c6aac26fb554205079
Connie Gao <connieg@mit.edu>
2016-07-06 12:37:20 -0400

    Add Intra_R_Add_Endocyclic and Intra_R_Add_Exocyclic training reactions
    From Wang, Zhang, and Zhang, 'Kinetics of Homoallylic/Homobenzylic
    Rearrangement Reactions under Combustion Conditions',
    J. Phys. Chem. A. 2014, 118, 6741-6748

--- input/kinetics/families/Intra_R_Add_Exocyclic/training/reactions.py
+++ input/kinetics/families/Intra_R_Add_Exocyclic/training/reactions.py
@@ -91,3 +91,83 @@ Taken from entry: prod2 <=> prod5
 """,
 )
 
+
+
+entry(
+    index = 7,
+    label = "C9H11 <=> C9H11-2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5.01e+07, 's^-1'), n=1.03, Ea=(14.13, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: ipso""",
+    longDesc = 
+u"""
+Taken from entry: R2 <=> M2
+""",
+)
+
+entry(
+    index = 8,
+    label = "C9H11-3 <=> C9H11-4",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5.25e+12, 's^-1'), n=0.19, Ea=(4.1, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: ipso""",
+    longDesc = 
+u"""
+Taken from entry: M2 <=> P2
+""",
+)
+
+entry(
+    index = 9,
+    label = "C10H13 <=> C10H13-2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(363000, 's^-1'), n=1.23, Ea=(17.56, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: ipso""",
+    longDesc = 
+u"""
+Taken from entry: R4 <=> M4
+""",
+)
+
+entry(
+    index = 10,
+    label = "C10H13-3 <=> C10H13-4",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(8.51e+12, 's^-1'), n=0.19, Ea=(9.77, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: ipso""",
+    longDesc = 
+u"""
+Taken from entry: M4 <=> P4
+""",
+)
+
+entry(
+    index = 11,
+    label = "C11H15 <=> C11H15-2",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(5370, 's^-1'), n=1.38, Ea=(8.62, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: ipso""",
+    longDesc = 
+u"""
+Taken from entry: R6 <=> M6
+""",
+)
+
+entry(
+    index = 12,
+    label = "C11H15-3 <=> C11H15-4",
+    degeneracy = 1,
+    kinetics = Arrhenius(A=(1.78e+13, 's^-1'), n=0.11, Ea=(18.4, 'kcal/mol'), T0=(1, 'K')),
+    rank = 3,
+    shortDesc = u"""Training reaction from kinetics library: ipso""",
+    longDesc = 
+u"""
+Taken from entry: M6 <=> P6
+""",
+)
+
